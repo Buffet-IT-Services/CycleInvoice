fdff842841c97d24cb5913d8ca5fc07e70a6927d
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-03-12 23:41:46 +0100

    refactored cycleInvoice to fit naming conventions

--- cycle_invoice/urls.py
+++ cycle_invoice/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cycleInvoice project.
+URL configuration for cycle_invoice project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cycleInvoice.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cycle_invoice.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
