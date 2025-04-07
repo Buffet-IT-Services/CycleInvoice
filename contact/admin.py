767ac537453db36fe4cd697a8e101a81f1f89b2d
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-04-07 22:56:15 +0200

    added models to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 """Register the Contact model with the admin site."""
 
-# Register your models here.
+from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+
+from .models import Address, CompanyContact, Contact, Organisation
+
+admin.site.register(Contact, SimpleHistoryAdmin)
+admin.site.register(Organisation, SimpleHistoryAdmin)
+admin.site.register(CompanyContact, SimpleHistoryAdmin)
+admin.site.register(Address, SimpleHistoryAdmin)
