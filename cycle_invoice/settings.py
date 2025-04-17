8d6ff5c8ae09e16daffdf49db66eb5902f2c62dd
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-04-17 08:40:55 +0200

    change to postgres

--- cycle_invoice/settings.py
+++ cycle_invoice/settings.py
@@ -80,9 +80,16 @@ WSGI_APPLICATION = "cycle_invoice.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'cycleinvoice',
+        'PASSWORD': 'cycleinvoice',
+        'HOST': '192.168.0.35',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=cycle_invoice'
+        },
     }
 }
 
