c83781f4f2d88e8cb1b4e956461b481782ac787c
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-05-21 08:30:22 +0200

    allow null for invoice in documentitem

--- sale/models.py
+++ sale/models.py
@@ -153,7 +153,7 @@ class DocumentItem(ChangeLoggerAll):
     quantity = models.DecimalField(max_digits=14, decimal_places=2, verbose_name=_("quantity"))
     discount = models.DecimalField(verbose_name=_("discount percent"), max_digits=5, decimal_places=4, default=0)
     item_group = models.CharField(max_length=255, verbose_name=_("item group"))
-    invoice = models.ForeignKey(DocumentInvoice, on_delete=models.CASCADE, related_name="document_item")
+    invoice = models.ForeignKey(DocumentInvoice, on_delete=models.CASCADE, related_name="document_item", null=True)
 
     @property
     @abstractmethod
@@ -168,7 +168,7 @@ class DocumentItem(ChangeLoggerAll):
     @property
     def price_str(self) -> str:
         """Return the price as a string."""
-        return f"{self.price:.2f}"
+        return f"{self.price}"
 
     @property
     def quantity_str(self) -> str:
