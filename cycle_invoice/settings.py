bda829c54dfe97165039a580be24eec3d96cde2d
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-06-07 13:52:44 +0200

    removed secret-key check

--- cycle_invoice/settings.py
+++ cycle_invoice/settings.py
@@ -26,6 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("The DJANGO_SECRET_KEY environment variable is not set or is empty.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
