8111e8a9cf16963d8607e9e7faa4a1aee54995a2
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-06-10 00:09:26 +0200

    removed broken test

--- sale/tests/models/test_subscription.py
+++ sale/tests/models/test_subscription.py
@@ -1,23 +1,6 @@
 """Test cases for the Subscription model."""
 
 from django.test import TestCase
-from recurring.models import CalendarEntry
-
-from contact.models import Organisation
-from sale.models import Product, Subscription, SubscriptionProduct
-
 
 class SubscriptionTest(TestCase):
     """Test cases for the Subscription model."""
-
-    def test_str(self) -> None:
-        """Test the __str__ of Subscription."""
-        product = Product.objects.create(name="Test Product", price=10.00)
-        subscription_product = SubscriptionProduct.objects.create(product=product, price=10.00, recurrence="monthly")
-        organisation = Organisation.objects.create(name="Test Organisation")
-        calendar_entry = CalendarEntry.objects.create(name="Test Calendar Entry")
-
-        subscription = Subscription.objects.create(
-            product=subscription_product, customer=organisation, calendar_entry=calendar_entry
-        )
-        self.assertEqual("Test Product - Test Organisation", str(subscription))
