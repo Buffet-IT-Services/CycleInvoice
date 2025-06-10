4eecb25e3145b35e9fea32acfe1e305d69237f02
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-06-10 11:14:56 +0200

    disabled logging for tests

--- cycle_invoice/settings.py
+++ cycle_invoice/settings.py
@@ -159,48 +159,48 @@ STORAGE = {
 
 LOG_DIR = BASE_DIR / "logs"
 LOG_DIR.mkdir(parents=True, exist_ok=True)
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-            "style": "{",
+if not "test" in sys.argv:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+            "simple": {
+                "format": "{levelname} {message}",
+                "style": "{",
+            },
         },
-        "simple": {
-            "format": "{levelname} {message}",
-            "style": "{",
+        "loggers": {
+            "django.utils.autoreload": {
+                "handlers": ["console"],
+                "level": "WARNING",
+                "propagate": False,
+            },
         },
-    },
-    "loggers": {
-        "django.utils.autoreload": {
-            "handlers": ["console"],
-            "level": "WARNING",
-            "propagate": False,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "simple",
+                "level": "ERROR",
+            },
+            "file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": LOG_DIR / "debug.log",
+                "formatter": "verbose",
+                "level": "INFO",
+                "mode": "w",
+                "maxBytes": 10485760,  # 10 MB
+                "backupCount": 5,  # Keep 5 backup files
+            },
         },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "simple",
-            "level": "ERROR",
-        },
-        "file": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": LOG_DIR / "debug.log",
-            "formatter": "verbose",
+        "root": {
+            "handlers": ["console", "file"],
             "level": "INFO",
-            "mode": "w",
-            "maxBytes": 10485760,  # 10 MB
-            "backupCount": 5,  # Keep 5 backup files
         },
-    },
-    "root": {
-        "handlers": ["console", "file"],
-        "level": "INFO",
-    },
-}
+    }
 
 # Celery settings
 CELERY_BROKER_URL = "redis://:foobared@192.168.0.37:6379/0"
