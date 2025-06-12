fb6eab4f43dd7da5a3fc116106a2d2bb867dd803
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-06-12 22:57:18 +0200

    disable S3 in Github Actions

--- cycle_invoice/settings.py
+++ cycle_invoice/settings.py
@@ -143,22 +143,26 @@ INTERNAL_IPS = [
 
 STORAGES = {
     "default": {
-        # Use S3Storage only if not running in GitHub Actions (CI)
+        # Use S3Storage only if not running in GitHub Actions (CI) or pytest
         "BACKEND": (
             "storages.backends.s3.S3Storage"
             if not (os.getenv("GITHUB_ACTIONS") == "true" or "pytest" in sys.modules)
             else "django.core.files.storage.FileSystemStorage"
         ),
-        "OPTIONS": {
-            "access_key": os.getenv("S3_ACCESS_KEY"),
-            "secret_key": os.getenv("S3_SECRET_KEY"),
-            "bucket_name": "cycleinvoice",
-            "endpoint_url": "https://minio.buffetitcloud.ch",
-            "addressing_style": "path",
-            "use_ssl": True,
-            "default_acl": None,
-            "signature_version": "s3v4",
-        },
+        "OPTIONS": (
+            {
+                "access_key": os.getenv("S3_ACCESS_KEY"),
+                "secret_key": os.getenv("S3_SECRET_KEY"),
+                "bucket_name": "cycleinvoice",
+                "endpoint_url": "https://minio.buffetitcloud.ch",
+                "addressing_style": "path",
+                "use_ssl": True,
+                "default_acl": None,
+                "signature_version": "s3v4",
+            }
+            if not (os.getenv("GITHUB_ACTIONS") == "true" or "pytest" in sys.modules)
+            else {}
+        ),
     },
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
