d39b72e69b14aa22297e3929a4a8f7074bc61818
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-06-17 22:58:41 +0200

    refined-urls

--- cycle_invoice/sale/urls.py
+++ cycle_invoice/sale/urls.py
@@ -1,11 +1,15 @@
 """URL configuration for the sale app."""
-from django.urls import path
+from django.urls import path, include
 
 from cycle_invoice.sale.api.document_invoice import InvoiceCreateApi, InvoiceDetailApi, InvoiceListApi, InvoiceUpdateApi
 
-urlpatterns = [
+invoice_patterns = [
     path("", InvoiceListApi.as_view(), name="document-invoice-list"),
     path("<int:pk>/", InvoiceDetailApi.as_view(), name="document-invoice-detail"),
     path("create/", InvoiceCreateApi.as_view(), name="document-invoice-create"),
     path("<int:pk>/update/", InvoiceUpdateApi.as_view(), name="document-invoice-update"),
+]
+
+urlpatterns = [
+    path("invoice/", include(invoice_patterns)),
 ]
