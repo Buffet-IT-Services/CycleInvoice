37447a9b20c1cca8297728c91b237dfad55342f7
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-06-19 01:26:01 +0200

    linked URLs directly

--- cycle_invoice/sale/tests/models/test_document_item.py
+++ cycle_invoice/sale/tests/models/test_document_item.py
@@ -161,6 +161,7 @@ class DocumentItemTest(TestCase):
 
         document_item.comment_description = None
         # TODO: Uncomment the following lines when the issue with comment_description is resolved
+        # https://github.com/Buffet-IT-Services/CycleInvoice/issues/56
         # Issue URL: https://github.com/Buffet-IT-Services/CycleInvoice/issues/56
         # try: noqa ERA001
         #     document_item.validate_constraints() noqa ERA001
