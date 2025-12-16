85ca2e67d93acbf60d6a00765c809c396cf8f1c9
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-12-16 22:19:44 +0100

    fix: updated settings

--- config/settings/jwt.py
+++ config/settings/jwt.py
@@ -3,9 +3,9 @@ import datetime
 import os
 
 JWT_AUTH = {
-    "JWT_SECRET_KEY": os.getenv("JWT_SECRET_KEY"),
+    "JWT_SECRET_KEY": os.getenv("JWT_SECRET_KEY") or os.getenv("DJANGO_SECRET_KEY"),
     "JWT_EXPIRATION_DELTA": datetime.timedelta(hours=6),
-    "JWT_ALLOW_REFRESH": os.getenv("JWT_ALLOW_REFRESH", "False"),
+    "JWT_ALLOW_REFRESH": os.getenv("JWT_ALLOW_REFRESH", "0").lower() in ("1", "true", "yes"),
     "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=1),
     "JWT_AUTH_COOKIE": os.getenv("JWT_AUTH_COOKIE", default="jwt"),
     "JWT_AUTH_COOKIE_SECURE": True,
