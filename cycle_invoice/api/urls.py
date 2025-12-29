a7abe38c5ed01cd30d14efeabba92635d33342c3
Florin Buffet <florin.buffet@buffetitservices.ch>
2025-12-29 22:12:06 +0100

    tests(Tasks): Ruff

--- cycle_invoice/api/urls.py
+++ cycle_invoice/api/urls.py
@@ -1,9 +1,11 @@
 """URLs for the api app."""
 from django.http import HttpRequest, JsonResponse
 from django.urls import path
+from django.views.decorators.http import require_safe
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 
+@require_safe
 def healthcheck(request: HttpRequest) -> JsonResponse:  # noqa: ARG001
     """Return a health check status for the API."""
     return JsonResponse({"status": "ok"}, status=200)
