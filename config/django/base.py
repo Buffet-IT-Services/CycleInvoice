cb61543a2713a8a9f5ed2c1525662865ca433879
Florin Buffet <florin.buffet@buffetitservices.ch>
2026-01-09 00:02:19 +0100

    feat(Docker): added staticfile dirs

--- config/django/base.py
+++ config/django/base.py
@@ -154,6 +154,9 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
