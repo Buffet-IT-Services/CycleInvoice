cd05c8614ad1e55acfff830c43f758a3768343c0
Florin Buffet <florin.buffet@buffetitservices.ch>
2026-01-09 00:17:32 +0100

    feat(Docker): favicon redirect

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("cycle_invoice.api.urls")),
     path("healthcheck/", include("health_check.urls")),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
 ]
